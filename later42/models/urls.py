55c4b5df01d391d7b11d0595c54c391af97a613d
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2022-10-27 12:39:11 +0300

    feat: unique email for users

--- later42/models/urls.py
+++ later42/models/urls.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+User._meta.get_field('email')._unique = True
+
 
 class URL(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
