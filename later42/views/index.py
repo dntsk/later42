34d6361be1e48c85d87413277ec921f7cef9cb1b
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2022-11-14 20:05:47 +0300

    fix warning

--- later42/views/index.py
+++ later42/views/index.py
@@ -28,12 +28,12 @@ def archive(request, url_id=None):
     try:
         urls = URL.objects.filter(
             user=request.user, archived=True).order_by('-id')
-        data = Article.objects.filter(url__in=urls).select_related('url')
+        data = Article.objects.filter(url__in=urls).select_related('url').order_by('-id')
         paginator = Paginator(data, settings.URLS_PER_PAGE)
         page_number = request.GET.get('page')
         data = paginator.get_page(page_number)
     except:
-        urls = []
+        data = []
     context = {'data': data}
     return render(request, 'archive.html', context)
 
