17255b2e563efb8f286fefabe0a6b7c1de3a7340
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2022-11-15 12:45:16 +0300

    Add URL validator

--- later42/views/api.py
+++ later42/views/api.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from later42.libs.content import get_content
+from django.core.validators import URLValidator
 from later42.models.urls import URL as URLModel
 from later42.tasks import get_url_content_task
 from django.conf import settings
@@ -8,7 +8,8 @@ from django.conf import settings
 
 class URL(APIView):
     def post(self, request, format=None):
-        if request.GET.get('url'):
+        val = URLValidator(verify_exists=False)
+        if val(request.GET.get('url')):
             get_url_content_task.delay(request.GET.get('url'), request.user.id)
             return Response({'status': 'success'})
         else:
